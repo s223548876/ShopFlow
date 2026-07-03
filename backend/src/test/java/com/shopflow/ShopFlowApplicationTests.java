5722075a98d13de18414de92912de8fbcb366083
Jeff <s223548876@gmail.com>
2026-07-03 12:26:24 +0800

    feat: add repository quality baseline

---
+++ backend/src/test/java/com/shopflow/ShopFlowApplicationTests.java
@@ -0,0 +1,12 @@
+package com.shopflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
